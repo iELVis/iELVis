bb54aa01c3ffafea33e08718d932782ffdcc656e
David Groppe <dgroppe@cogsci.ucsd.edu>
2017-09-01 22:34:34 -0400

    get_PTDindx.m->getPtdIndex.m
    Needed to make a couple of coordinate changes to get_PTDindx.m to make it compatible with LEPTOVOX files.

--- iELVis_MAIN/iELVis_MATLAB/ELEC_LOC/getPtdIndex.m
+++ iELVis_MAIN/iELVis_MATLAB/ELEC_LOC/getPtdIndex.m
@@ -1,45 +1,56 @@
-function WandG = get_PTDindx(fs_subj)
+function PTD_idx = getPtdIndex(fs_subj)
+% function PTD_idx = getPtdIndex(fs_subj)
 %
-% compute Proximal Tissue Density for each electrode
-% PTD is an index reflect the density of gray and white matter surrounding
-% a stereotactic electrode implanted in the brain.
-% as describe in: Mercier et al., Neuroimage 2017
+% Compute Proximal Tissue Density (PTD) for each electrode as described in 
+% Mercier et al., Neuroimage 2017
+%
+% PTD is an index reflecting the density of neocortical gray and white matter surrounding
+% a stereotactic electrode that has its centroid either in the neocortical Gray or in the 
+% White matter. (otheriwse PTD = NaN)
+%
+% Be careful when a contact is in the vicinity of subcortical structures
+% PTD is computed exclusively by taking into account surrounding neocortical
+% Gray and White matter voxels; no other tissue will be taken into account (e.g. 
+% Hippocampus, Amygdala...). Voxel labels are taken from the FreeSurfer
+% wmparc.mgz file.
 % 
-% Author: Manuel Mercier (manuel.mercier@a3.epfl.ch)
+% Please note that this function belongs to the iELVis toolbox
+% and is therefore subjected to the same regulations
+% (Author: manuel.mercier@a3.epfl.ch)
 %
-% input: fs_subj name of the subject in the FS folder
+% input: fs_subj - name of the patient's FreeSurfer subject folder
 %
-% output: WandG strcture containing 
-%           >> elec: {nx1 cell} (from 
+% output: PTD_idx structure containing 
+%           >> elec: {nx1 cell}
 %           >> location: {nx1 cell}
 %           >> offset: 2
 %           >> nb_Gpix [nx1 double]
 %           >> nb_Wpix [nx1 double]
-%           >> WandG.PTD [nx1 double]
+%           >> PTD_idx.PTD [nx1 double]
 %
 % with:
-% - Offset correspond to the size of the cube (default = 2)
-% - nb_Gpix and nb_Wpix correspond to the number of Gray or White matter voxels
-%   within a cube centered around the electrode centroid
+% - elec: electrodes name
+% - location: brain region where the centroid of each electrode is localized based on freesurfer parcellation
+% - Offset: correspond to the size of the cube around each electrode used to approximate the PTD (default = 2)
+% - nb_Gpix and nb_Wpix correspond to the number of Gray or White matter voxels within a cube centered around the electrode centroid
 %   (default size = 27 ;centroid plus offset)
-% - PTD proximal tissue density of gray and White matter electrode around the electrode
+% - PTD proximal tissue density of white and neocortical gray matter around the electrode
+%
+% In addition the following files are created in the elec_recon subfolder 
+% of the patient's FreeSurfer directory:
+%   -GreyWhite_classifications.mat
+%   -GreyWhite_classifications.txt
 %
 %
-% functions needed:
-% - MRIread from freesurfer
-% - euclid_dist_mtx from epiproc
+% Function dependency:
+% - MRIread from freesurfer (https://surfer.nmr.mgh.harvard.edu/)
 % 
-% files needed:
+% Files needed:
 % - MRI from elec_recon (wmparc.mgz)
-% - electrodes coordinates (*.3dUndump.VOX)
+% - electrodes coordinates (*.LEPTOVOX)
 % - electrodes names (*.electrodeNames)
 % - parcellation code table (FreeSurferColorLUT.txt)
-% - information about channel (*_channelinfo.mat)
 % 
-% if hemispheres were processed separatlty,
-% use the last section of the script to merge
-% files from Left and Right infos ...
-%
 %
 % Reference:
 % Mercier, M. R., Bickel, S., Megevand, P., Groppe, D. M., Schroeder, C. E., 
@@ -48,75 +59,57 @@ function WandG = get_PTDindx(fs_subj)
 % A glimpse on white matter signal. NeuroImage, 147, 219-232.
 
 % Change Log:
-% 08-2017: addapted for iElvis by MrM;
+% 08-2017: a few other minor changes for iELVis by DG
+% 08-2017: adapted for iElvis by MrM;
 % 02-2016: created by MrM;
 %
 
-
-%load parcellation file
-if isunix || ismac
-fs_dir=getenv('SUBJECTS_DIR');
-recon_folder=fullfile(fs_dir,fs_subj,'elec_recon/');
+% load parcellation file
+fs_dir=getFsurfSubDir();
+recon_folder=fullfile(fs_dir,fs_subj,'elec_recon');
 parc_file=fullfile(fs_dir,fs_subj,'mri','wmparc.mgz');
 mri=MRIread(parc_file);
-elseif ispc
-fs_dir=uigetdir(userpath,'select freesurfer directory');
-recon_folder=fullfile(fs_dir,fs_subj,'elec_recon\');
-parc_file=fullfile(fs_dir,fs_subj,'mri','wmparc.mgz');
-mri=MRIread([parc_file(1:end-4) '.mgz']);
-end        
-%load coordinates
-% files=dir([recon_folder '*.3dUndump.VOX']);
-files=dir([recon_folder '*.LEPTOVOX']);
 
-n=size(files,1);
-if n==1
-    elec_file=fullfile(recon_folder,files.name);
-elseif n==0
-    disp('No 3dUndump file found. Please do it manualy');
-    [temp_file,elec_dir]=uigetfile([recon_folder '*.LEPTOVOX'],'select electrode coordinate file');
-    elec_file=fullfile(elec_dir,temp_file);    
-    clear elec_dir temp_file    
-elseif n>1
-    disp('More than one 3dUndumpfile found. Please choose it manualy');
-    [temp_file,elec_dir]=uigetfile([recon_folder '*.LEPTOVOX'],'select electrode coordinate file');
-    elec_file=fullfile(elec_dir,temp_file);
-    clear elec_dir temp_file
-end
-%load electrodes names
-files=dir([recon_folder '*.electrodeNames']);
+% load electrodes name
+files=dir(fullfile(recon_folder,'*.electrodeNames'));
 n=size(files,1);
 if n==1
     label_file=fullfile(recon_folder,files.name);
 elseif n==0
     disp('No electrodeNames file found. Please do it manualy.');
-    [temp_file,elec_dir]=uigetfile([recon_folder '*.electrodeNames'],'select electrode names file');
+    [temp_file,elec_dir]=uigetfile(fullfile(recon_folder,'*.electrodeNames'),'select electrode names file');
     label_file=fullfile(elec_dir,temp_file);
-    clear elec_dir temp_file    
+    clear elec_dir temp_file files n
 elseif n>1
     disp('More than one electrodeNames file found. Please do it manualy.');
-    [temp_file,elec_dir]=uigetfile([recon_folder '*.electrodeNames'],'select electrode names file');
+    [temp_file,elec_dir]=uigetfile(fullfile(recon_folder,'*.electrodeNames'),'select electrode names file');
     label_file=fullfile(elec_dir,temp_file);
-    clear elec_dir temp_file
+    clear elec_dir temp_file files n
 end
-
-%% load look-up table for the FreeSurfer MRI atlases
-FS_color_file = which('FreeSurferColorLUTnoformat.txt');
-if isempty(FS_color_file)
-    disp('the freesurfer color code file was not found. Please select it manualy.');
-    [temp_file,elec_dir]=uigetfile([recon_folder '*.txt'],'select  freesurfer color code file');
-    FS_color_file=fullfile(elec_dir,temp_file);
-    clear elec_dir temp_file
-end
-    
-fid=fopen(FS_color_file);
-C=textscan(fid,'%d %s %d %d %d %d');
+fid=fopen(label_file);
+tmp=textscan(fid,'%s %s %s');
 fclose(fid);
+for i=3:length(tmp{1})
+    label{i-2,1}=strcat(tmp{1}{i},'_',tmp{2}{i},'_',tmp{3}{i});
+end
+clear tmp;
 
-region_lookup=C{:,2};
-region_codes=C{:,1};
-clear fid C p
-%% load electrode coordinates
+% load electrodes coordinate
+files=dir(fullfile(recon_folder,'*.LEPTOVOX'));
+n=size(files,1);
+if n==1
+    elec_file=fullfile(recon_folder,files.name);
+elseif n==0
+    disp('No *.LEPTOVOX file found. Please do it manualy');
+    [temp_file,elec_dir]=uigetfile(fullfile(recon_folder,'*.LEPTOVOX'),'select electrode coordinate file');
+    elec_file=fullfile(elec_dir,temp_file);    
+    clear elec_dir temp_file files n
+elseif n>1
+    disp('More than one *.LEPTOVOX file found. Please choose it manualy');
+    [temp_file,elec_dir]=uigetfile(fullfile(recon_folder,'*.LEPTOVOX'),'select electrode coordinate file');
+    elec_file=fullfile(elec_dir,temp_file);
+    clear elec_dir temp_file files n
+end
 fid=fopen(elec_file);
 tmp=textscan(fid,'%s %s %s');
 fclose(fid);
@@ -126,101 +119,136 @@ for i=3:length(tmp{1})
     elec(i-2,3)=str2num(tmp{3}{i});
 end
 clear tmp;
+elec=elec+1; % Voxel indexing starts at 0 but MATLAB indexing starts with 1
 
-% load electrode labels
-fid=fopen(label_file);
-tmp=textscan(fid,'%s %s %s');
-fclose(fid);
-for i=3:length(tmp{1})
-    label{i-2,1}=strcat(tmp{1}{i},'_',tmp{2}{i},'_',tmp{3}{i});
+%% load look-up table for the FreeSurfer MRI atlases
+FS_color_file = which('FreeSurferColorLUTnoFormat.txt');
+if isempty(FS_color_file)
+    disp('The freesurfer color code file, FreeSurferColorLUTnoFormat.txt, was not found. Please select it manualy.');
+    [temp_file,elec_dir]=uigetfile(fullfile(recon_folder,'*.txt'),'Select freesurfer color code file');
+    FS_color_file=fullfile(elec_dir,temp_file);
+    clear elec_dir temp_file
+else
+   fprintf('Loading file %s\n',FS_color_file); 
 end
-clear tmp;
 
+fid=fopen(FS_color_file);
+C=textscan(fid,'%d %s %d %d %d %d');
+fclose(fid);
 
+region_lookup=C{:,2};
+region_codes=C{:,1};
+clear fid C p
 
-%% find the proportion of grey and white matter surrounding the electrodes
-
+%% find the proportion of neocortical grey and white matter surrounding the electrodes
+sVol=size(mri.vol);
 offset = 2;
 for e=1:size(elec,1)
     disp(['Finding amount of surrounding grey and white matter for channel ' label{e}]);
-    x=round(elec(e,2)); % switches x and y planes to match FreeSurfer
-    y=round(elec(e,1)); % switches x and y planes to match FreeSurfer
-    z=round(elec(e,3));
+    x=round(elec(e,2)); % switches x and y axes to match FreeSurfer
+    y=round(elec(e,1)); % switches x and y axes to match FreeSurfer
+    z=round(sVol(3)-elec(e,3)); %need to flip direction of last coordinate
+    
+    % DG Code for double checking electrode coordinates
+    if e==0, %68, 61
+        mri_mn=min(min(min(mri.vol)));
+        mri_mx=max(max(max(mri.vol)));
+        
+        figure(2)
+        colormap('gray');
+        clf;
+        subplot(1,3,1);
+        imagesc(squeeze(mri.vol(:,y,:)),[mri_mn, mri_mx]);
+        hold on;
+        axis square;
+        set(gca,'xdir','reverse');
+        plot(z,x,'g.');
+        
+        subplot(1,3,2);
+        imagesc(squeeze(mri.vol(x,:,:)),[mri_mn, mri_mx]);
+        hold on;
+        axis square;
+        set(gca,'xdir','reverse');
+        plot(z,y,'g.');
+        
+       subplot(1,3,3);
+       imagesc(squeeze(mri.vol(:,:,z)),[mri_mn, mri_mx]);
+       hold on;
+       axis square;
+       plot(y,x,'g.');
+       
+       disp('Done.');
+    end
     
     % original labelling from parcellation
     ROI(e,1) =region_lookup(region_codes==mri.vol(x,y,z));
-    % get the euclidean distances between the electrode and every voxel in the MRI
-    for i=1:mri.volsize(1)
-        for j=1:mri.volsize(2)
-            for k=1:mri.volsize(3)
-                distances(i,j,k)=sqrt((i-x)^2+(j-y)^2+(k-z)^2);
+    
+    % check that the contact is in gray or white matter
+    gray_white=[strfind(lower(ROI{e,1}),'ctx') strfind(lower(ROI{e,1}),'cortex') ...
+        strfind(lower(ROI{e,1}),'wm') strfind(lower(ROI{e,1}),'white')];
+    if isempty(gray_white),
+        % get the euclidean distances between the electrode and every voxel in the MRI
+        for i=1:mri.volsize(1)
+            for j=1:mri.volsize(2)
+                for k=1:mri.volsize(3)
+                    distances(i,j,k)=sqrt((i-x)^2+(j-y)^2+(k-z)^2); % could be replaced by ~ norm([i j k] - [x y z])
+                end
             end
         end
+
+        % do not include the offset
+        tmp=mri.vol(distances<offset); 
+        % find the regions
+        tmp_region ={};
+            for i = 1:length(tmp)
+               tmp_region(i,1) =region_lookup(region_codes==tmp(i));  
+            end
+        % find gray matter voxel in the vicinity
+        gm_nb = length(cell2mat(strfind(lower(tmp_region),'ctx')));
+        gm_nb = gm_nb + length(cell2mat(strfind(lower(tmp_region),'cortex')));
+        gm_w = gm_nb;
+        % find white matter voxel in the vicinity
+        wm_nb = length(cell2mat(strfind(lower(tmp_region),'wm')));
+        wm_nb = wm_nb + length(cell2mat(strfind(lower(tmp_region),'white')));
+        wm_w = wm_nb;
+
+        ROI{e,2} = gm_w;
+        ROI{e,3} = wm_w;
+    else
+        warning(['channel ' label{e} ' does not have its centroid in neither Gray or White matter >> PTD=NaN']);
+        ROI{e,2} = NaN;
+        ROI{e,3} = NaN;
     end
-    
-    % do not include the offset
-    tmp=mri.vol(distances<offset); 
-    % find the regions
-    tmp_region ={};
-        for i = 1:length(tmp)
-           tmp_region(i,1) =region_lookup(region_codes==tmp(i));  
-        end
-    % find gray matter voxel in the vicinity
-    gm_nb = length(cell2mat(strfind(lower(tmp_region),'ctx')));
-    gm_nb = gm_nb + length(cell2mat(strfind(lower(tmp_region),'cortex')));
-    gm_w = gm_nb;
-    % find white matter voxel in the vicinity
-    wm_nb = length(cell2mat(strfind(lower(tmp_region),'wm')));
-    wm_nb = wm_nb + length(cell2mat(strfind(lower(tmp_region),'white')));
-    wm_w = wm_nb;
-    
-    ROI{e,2*(offset-1)} = gm_w;
-    ROI{e,3*(offset-1)} = wm_w;
 end
 %% write output file
 
 for i=1:length(ROI)   
-    WandG.elec(i,1)     = label(i);
-    WandG.location(i,1) = ROI(i,1);
-    WandG.nb_Gpix(i,1)  = cell2mat(ROI(i,2));
-    WandG.nb_Wpix(i,1)  = cell2mat(ROI(i,3));
-    WandG.PTD (i,1)     = (cell2mat(ROI(i,2)) - cell2mat(ROI(i,3))) / (cell2mat(ROI(i,2)) + cell2mat(ROI(i,3)));
+    PTD_idx.elec(i,1)     = label(i);
+    PTD_idx.location(i,1) = ROI(i,1);
+    PTD_idx.nb_Gpix(i,1)  = cell2mat(ROI(i,2));
+    PTD_idx.nb_Wpix(i,1)  = cell2mat(ROI(i,3));
+    PTD_idx.PTD (i,1)     = (cell2mat(ROI(i,2)) - cell2mat(ROI(i,3))) / (cell2mat(ROI(i,2)) + cell2mat(ROI(i,3)));
+    if (cell2mat(ROI(i,2)) + cell2mat(ROI(i,3))) ~= power(offset+1,3)
+       warning(['channel ' label{e} ' has in its surrounding voxels that are neither labelled Gray or White matter; ' char(10)...
+           'those voxels were not taking into account in PTD computation (see field nb_Gpix and nb_Wpix in the output)']);
+    end
+% % otherwise a less strict version of the PTD taking into account the surrounding voxels that do not belong to Gray or White matter    
+%     PTD_idx.PTD (i,1)     = (cell2mat(ROI(i,2)) - cell2mat(ROI(i,3))) / power(offset+1,3);
 end
-WandG.offset = offset;
-%save
-save([recon_folder 'GreyWhite_classifications.mat'],'WandG');
-% raw output in txt
-outfile=([recon_folder 'GreyWhite_classifications.txt']);
+PTD_idx.offset = offset;
+
+% Save PTD info as mat file
+outfile_mat=fullfile(recon_folder,'GreyWhite_classifications.mat');
+fprintf('Writing PTD values to %s\n',outfile_mat);
+save(outfile_mat,'PTD_idx');
+
+% Raw output in txt
+outfile=fullfile(recon_folder,'GreyWhite_classifications.txt');
+fprintf('Writing PTD values to %s\n',outfile);
 fid=fopen(outfile,'w');
 fprintf(fid,'%s\t %s\t %s\t %s\r\n','Electrode','location','% Grey with offset=2','% White with offset=2');
 for i=1:size(ROI,1)
     fprintf(fid,'%s\t %s\t %d\t %d\t \r\n',label{i},ROI{i,:});
 end
 fclose(fid);
-
-
-
-%% if separate R/L files you can run this piece of code to merge the L/R outputfiles
-% 
-% for i = 1:length(WandG_L.elec)
-%     if ~isempty(WandG_L.elec{i});
-%         WandG.elec{i,1}       = WandG_L.elec{i};
-%         WandG.location{i,1}   = WandG_L.location{i};
-%         WandG.G1(i,1)       = WandG_L.G1(i);
-%         WandG.W1(i,1)       = WandG_L.W1(i);
-% %         WandG.G2(i,1)       = WandG_L.G2(i);
-% %         WandG.W2(i,1)       = WandG_L.W2(i);
-% %         WandG.G3(i,1)       = WandG_L.G3(i);
-% %         WandG.W3(i,1)       = WandG_L.W3(i); 
-%     elseif isempty(WandG_L.elec{i}) && i <= length(WandG_R.elec);
-%         WandG.elec{i,1}       = WandG_R.elec{i};
-%         WandG.location{i,1}   = WandG_R.location{i};
-%         WandG.G1(i,1)       = WandG_R.G1(i);
-%         WandG.W1(i,1)       = WandG_R.W1(i);
-% %         WandG.G2(i,1)       = WandG_R.G2(i);
-% %         WandG.W2(i,1)       = WandG_R.W2(i);
-% %         WandG.G3(i,1)       = WandG_R.G3(i);
-% %         WandG.W3(i,1)       = WandG_R.W3(i); 
-%     end
-% end
 end
